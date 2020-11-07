LineageOS/android_frameworks_base

power: Re-introduce custom charging sounds
 * Mostly inspired by old implementation (see commit 72fe399),
   but refactored to use most of the AOSP code.

 * Also adapted to the wireless charging feature introduced in P
   (note that it was assumed that we should not play any vibration
   when the device is undocked from the wireless charger).

Change-Id: I5f6589002db51fe03ec2cf7bb33ed0e63948c55a

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1782,6 +1782,8 @@ public final class PowerManagerService extends SystemService
                     if (mIsPowered && !BatteryManager.isPlugWired(oldPlugType)
                             && BatteryManager.isPlugWired(mPlugType)) {
                         mNotifier.onWiredChargingStarted(mForegroundProfile);
+                    } else if (wasPowered && !mIsPowered && !dockedOnWirelessCharger) {
+                        mNotifier.onWiredChargingDisconnected(mForegroundProfile);
                     } else if (dockedOnWirelessCharger) {
                         mNotifier.onWirelessChargingStarted(mBatteryLevel, mForegroundProfile);
                     }

Introdructor/android_frameworks_base-2

 * Also adapted to the wireless charging feature introduced in P.
 Also include:
   power: Respect global vibration setting for charging sounds
    * Don't vibrate when in silent mode
   Change-Id: Ife14105e74a28856d74a4c153637344124686e1b
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/power/PowerManagerService.java
@@ -2035,6 +2035,8 @@ public final class PowerManagerService extends SystemService
                     if (mIsPowered && !BatteryManager.isPlugWired(oldPlugType)
                             && BatteryManager.isPlugWired(mPlugType)) {
                         mNotifier.onWiredChargingStarted(mUserId);
+                    } else if (wasPowered && !mIsPowered) {
+                        mNotifier.onWiredChargingDisconnected(mUserId);
                     } else if (dockedOnWirelessCharger) {
                         mNotifier.onWirelessChargingStarted(mBatteryLevel, mUserId);
                     }